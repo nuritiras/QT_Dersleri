a937bbb6b51c50aca9555aaf11639ca8a91cc7a3
Nuri TIRAŞ <nuritiras@gmail.com>
2022-05-28 10:20:02 +0300

    Arayüz çalışması yapıldı.

--- Giris/mainwindow.cpp
+++ Giris/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->pushButton->setIcon(QIcon(":/icons/icons/Hello.ico"));
 }
 
 MainWindow::~MainWindow()
